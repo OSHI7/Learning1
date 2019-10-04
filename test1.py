bca5833581bb36bbeb3c6dbd7e6cb9b662e9c91f
Josh (Ativ9) <philipson00@gmail.com>
2019-10-03 23:18:02 -0400

    implement greeting in test2.py

--- test1.py
+++ test1.py
@@ -3,8 +3,19 @@ list=['happy', 'sad', 'quick', 'slow']
 
 # for item in list:
 #     print(item)
+import numpy as np
 
 i=iter(list)
 print(i())
 print(i)
+x = np.array([2,3,1,0])
+y=x^2
 
+# Create an 1d array from a list
+import numpy as np
+list1 = [0,1,2,3,4]
+arr1d = np.array(list1)
+
+# Print the array and its type
+print(type(arr1d))
+arr1d
