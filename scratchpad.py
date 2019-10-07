eb14f3993b5dac977ef274f68a82e64e8aec9149
Joshua Philipson <Joshua.Philipson@viavisolutions.com>
2019-10-07 13:37:39 -0400

    adding PlaygroundStuff.txt, ignoring vsCode

---
+++ scratchpad.py
@@ -0,0 +1,23 @@
+import numpy as np  
+import matplotlib 
+import matplotlib.pyplot as plt 
+
+#list=['happy', 'sad', 'quick', 'slow']
+#i=iter(list)
+#print(i())
+#print(i)
+#print(list) 
+#print(list[1]) 
+print(1+1)
+print('2')
+print(3*4)
+
+x=np.arange(-10,10, 0.01) 
+print(x)
+print('hi')
+
+# plt.figure()
+# plt.plot(x, x**2) 
+# plt.show()
+
+plt.plot(x, np.cos(x/100))
