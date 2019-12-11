863840ed705903e3dee31992eb569f691532074a
joshua-PC\joshua <philipson00@gmail.com>
2019-12-11 00:04:35 -0500

    adding notes to keyboard shortcuts

--- Analysis-On-My-Words.py
+++ Analysis-On-My-Words.py
@@ -1,5 +1,22 @@
 # 'pip install nb2xls ' - EXCEL reference; https://towardsdatascience.com/jupyter-is-the-new-excel-but-not-for-your-boss-d24340ebf314
 
 import pandas as pd
+import os
 
-print('done')
+##
+fname='words.xlsx'
+cwd= os.getcwd() # current working directory
+
+Fname=os.path.join(cwd, fname)
+
+
+
+# # pd.
+# # print('done')Q
+
+##
+print('cell 1')
+
+
+##
+print('cell2')
